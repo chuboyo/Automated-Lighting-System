7adffa292c33ec1622fc3c264e2ad1c51f2c8175
chuboyo <akojichubiyojo1997@gmail.com>
2022-08-16 19:50:25 +0100

    automated lighting system completed

---
+++ Automated_Lighting.ino
@@ -0,0 +1,56 @@
+#include <LowPower.h>
+#include <EEPROM.h>
+
+/* 
+ *  Automated Lighting Control System
+ *  By Akoji Timothy and Akoji Chubi
+ *  16th May, 2022
+ 
+ *  This system automatically turns ON a lighting source based on the current amount of daylight brightness
+ *  
+ *  Inputs: Light Dependent Resistor (Analog) connected to A0
+ *  Outputs: Indicator LED (Pin 7), Relay Module (pin 8)
+*/
+// Variable Definitions
+int LDR_sensor = A1;
+int relay_pin = 10;
+int LED_pin = 6;
+int address = 0;
+// Define the rest here 
+
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  pinMode(LDR_sensor, INPUT); // set A0 as input
+  pinMode(relay_pin, OUTPUT);
+  pinMode(LED_pin, OUTPUT);
+  digitalWrite(relay_pin, LOW);
+  digitalWrite(LED_pin, HIGH); 
+  
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+      int LDR_reading = analogRead(LDR_sensor)/4;
+      // place the rest of the code here:
+      if (LDR_reading < 200 ){
+          digitalWrite(relay_pin, HIGH);
+        }
+      else {
+        digitalWrite(relay_pin, LOW);
+        }
+      
+      // print out the state of the button:
+      Serial.println(LDR_reading);
+      Serial.println("*****************");
+      if (address < EEPROM.length()){
+        EEPROM.write(address, LDR_reading);
+        address += 1; 
+        }
+             // Get reading every 10 seconds
+  for (int i = 0; i < 300; i++){
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+
+} 
